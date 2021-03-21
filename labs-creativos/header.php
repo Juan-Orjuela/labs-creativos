b2ab1e81598c0c7bf63a1162f56f87d9593dd216
Juan Orjuela <orjuelajuans@gmail.com>
2021-03-21 17:51:32 -0500

    Theme Wordpress

---
+++ labs-creativos/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Laboratorios creativos</title>
+    <!--<link rel="stylesheet" href="css/estilos.css" />
+    <link rel="stylesheet" href="css/estilos-probetas.css" />
+    <link rel="stylesheet" href="css/estilos-elementos-flotantes.css" />-->
+    <link rel=”stylesheet” href=”<?php echo get_template_directory_uri(); ?>/style.css” type=”text/css” media=”all” />.
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.js"
+      integrity="sha512-Gx/C4x1qubng2MWpJIxTPuWch9O88dhFFfpIl3WlqH0jPHtCiNdYsmJBFX0q5gIzFHmwkPzzYTlZC/Q7zgbwCw=="
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
+    <?php wp_head();?>
+  </head>
+  <body>
